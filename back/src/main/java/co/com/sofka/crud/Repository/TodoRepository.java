17f0ddb875ce39b4ffb054cb5d54c4926e653b9d
Mauricio Torres <otarksk@gmail.com>
2022-02-28 14:46:15 -0300

    Metodo que no se usa en el back eliminado

--- back/src/main/java/co/com/sofka/crud/Repository/TodoRepository.java
+++ back/src/main/java/co/com/sofka/crud/Repository/TodoRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
-    //List<Todo> findAllTodoList(Long groupListId);
 }
